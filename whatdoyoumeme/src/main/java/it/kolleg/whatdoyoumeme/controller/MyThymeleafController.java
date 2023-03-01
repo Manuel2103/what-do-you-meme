c3707a178a34f74d398017d6ed61a39abf0aee5e
ManuelFoidl <manufoidl@tsn.at>
2023-03-01 11:06:56 +0100

    Picture add and change

--- whatdoyoumeme/src/main/java/it/kolleg/whatdoyoumeme/controller/MyThymeleafController.java
+++ whatdoyoumeme/src/main/java/it/kolleg/whatdoyoumeme/controller/MyThymeleafController.java
@@ -12,7 +12,9 @@ import it.kolleg.whatdoyoumeme.services.QuoteService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -49,20 +51,64 @@ public class MyThymeleafController {
         memeService.setzeLike(id);
         return new RedirectView("/web/allmemes");
     }
-
+    @GetMapping("web/deletememe/{id}")
+    public View deleteMeme(@PathVariable long id) throws MemeNotFound {
+        memeService.loescheMemeMitID(id);
+        return new RedirectView("/web/allmemes");
+    }
     @GetMapping("web/allpictures")
     public ModelAndView allPictures(){
         List<Picture> pictures = pictureService.gibAllePicture();
         return new ModelAndView("allpictures", "pictures", pictures);
     }
     @GetMapping("web/addmeme")
-    public ModelAndView addMeme() throws QuoteNotFound, PictureNotFound {
+    public ModelAndView addMemeForm() throws QuoteNotFound, PictureNotFound {
         List<Quote> randomQuotes = quoteService.gib4RandomQuotes();
         Picture randomPicture = pictureService.gibRandomPicture();
         ModelAndView modelAndView = new ModelAndView("addmeme");
         modelAndView.addObject("quotes", randomQuotes);
         modelAndView.addObject("picture", randomPicture);
+        modelAndView.addObject("meme", new Meme());
         return modelAndView;
+    }
+    @GetMapping("web/addpicture")
+    public ModelAndView addPictureForm(){
+        return new ModelAndView("addpicture", "picture", new Picture());
+    }
+
+    @PostMapping("web/addpicture")
+    public String addPicture(@Valid @ModelAttribute("picture") Picture picture, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "addpicture";
+        }else{
+            pictureService.speicherePicture(picture);
+            return "redirect:/web/allpictures";
+        }
+    }
+    @GetMapping("web/changepicture/{id}")
+    public ModelAndView changePictureForm(@PathVariable Long id) throws PictureNotFound {
+        return new ModelAndView("changepicture", "picture", pictureService.gibPictureMitId(id));
+    }
+    @PostMapping("web/changepicture")
+    public String changePicture(@Valid @ModelAttribute("picture") Picture picture, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "changepicture";
+        }else{
+            pictureService.aktualisierePicture(picture);
+            return "redirect:/web/allpictures";
+        }
+
+
+    }
+
+    @PostMapping("web/addmeme")
+    public String addMeme(@Valid @ModelAttribute("meme") Meme meme, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "addmeme";
+        }else{
+            memeService.speichereMeme(meme);
+            return "redirect:/web/allmemes";
+        }
     }
 
 }
